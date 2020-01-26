d59aad6829a765eeac0ddaed4da50a86d0ad168a
cambalamas <cambalamas@gmail.com>
2020-01-26 11:43:31 +0100

    deploy v2, ADD: animated intro, better aboutme & new favicons

--- index.js
+++ index.js
@@ -1,316 +1,143 @@
-// ----------------------- //
-// Daniel Camba Lamas
-// <cambalamas@gmail.com>
-// ----------------------- //
+// --- 3rd party ---
+// ------------------------------------------------------------------------- //
 
-// HELPERS
+// YALL
+var yall = function () { "use strict"; return function (e) { var n = (e = e || {}).lazyClass || "lazy", t = e.lazyBackgroundClass || "lazy-bg", o = "idleLoadTimeout" in e ? e.idleLoadTimeout : 200, i = e.observeChanges || !1, r = e.events || {}, a = window, s = "requestIdleCallback", u = "IntersectionObserver", c = ["srcset", "src", "poster"], d = [], queryDOM = function (e, o) { return d.slice.call((o || document).querySelectorAll(e || "img." + n + ",video." + n + ",iframe." + n + ",." + t)) }, yallLoad = function (n) { var o = n.parentNode; "PICTURE" == o.nodeName && yallApplyFn(queryDOM("source", o), yallFlipDataAttrs), "VIDEO" == n.nodeName && yallApplyFn(queryDOM("source", n), yallFlipDataAttrs), yallFlipDataAttrs(n), n.autoplay && n.load(); var i = n.classList; i.contains(t) && (i.remove(t), i.add(e.lazyBackgroundLoaded || "lazy-bg-loaded")) }, yallBindEvents = function (e) { for (var n in r) e.addEventListener(n, r[n].listener || r[n], r[n].options || void 0) }, yallFlipDataAttrs = function (e) { var _loop = function (n) { c[n] in e.dataset && a.requestAnimationFrame((function () { e.setAttribute(c[n], e.dataset[c[n]]) })) }; for (var n in c) _loop(n) }, yallApplyFn = function (e, n) { for (var t = 0; t < e.length; t++)n instanceof a[u] ? n.observe(e[t]) : n(e[t]) }, yallIntersectionObserve = function (e) { if (e.isIntersecting || e.intersectionRatio) { var t = e.target; s in a && o ? a[s]((function () { yallLoad(t) }), { timeout: o }) : yallLoad(t), t.classList.remove(n), f.unobserve(t), (l = l.filter((function (e) { return e != t }))).length || i || f.disconnect() } }, yallMutationObserve = function (e) { l.indexOf(e) < 0 && (l.push(e), yallBindEvents(e), f.observe(e)) }, l = queryDOM(); if (/baidu|(?:google|bing|yandex|duckduck)bot/i.test(navigator.userAgent)) yallApplyFn(l, yallLoad); else if (u in a && u + "Entry" in a) { var f = new a[u]((function (e) { yallApplyFn(e, yallIntersectionObserve) }), { rootMargin: ("threshold" in e ? e.threshold : 200) + "px 0%" }); yallApplyFn(l, yallBindEvents), yallApplyFn(l, f), i && yallApplyFn(queryDOM(e.observeRootSelector || "body"), (function (n) { new MutationObserver((function () { yallApplyFn(queryDOM(), yallMutationObserve) })).observe(n, e.mutationObserverOptions || { childList: !0, subtree: !0 }) })) } } }();
 
-/**
- * Check the current scroll position using 'window' or 'document' as fallback.
- */
-function currScrollPos() {
-    return window.pageYOffset || document.documentElement.scrollTop;
-}
+// SmtpJS
+var Email = { send: function (a) { return new Promise(function (n, e) { a.nocache = Math.floor(1e6 * Math.random() + 1), a.Action = "Send"; var t = JSON.stringify(a); Email.ajaxPost("https://smtpjs.com/v3/smtpjs.aspx?", t, function (e) { n(e) }) }) }, ajaxPost: function (e, n, t) { var a = Email.createCORSRequest("POST", e); a.setRequestHeader("Content-type", "application/x-www-form-urlencoded"), a.onload = function () { var e = a.responseText; null != t && t(e) }, a.send(n) }, ajax: function (e, n) { var t = Email.createCORSRequest("GET", e); t.onload = function () { var e = t.responseText; null != n && n(e) }, t.send() }, createCORSRequest: function (e, n) { var t = new XMLHttpRequest; return "withCredentials" in t ? t.open(e, n, !0) : "undefined" != typeof XDomainRequest ? (t = new XDomainRequest).open(e, n) : t = null, t } };
 
-/**
- * Gived a html string generate html nodes
- */
-function str2html(html) {
-    var template = document.createElement('template');
-    template.innerHTML = html;
-    return template.content.childNodes;
-}
+// ------------------------------------------------------------------------- //
+// ---------------------------------------------- //
+// --------------------- //
 
-/**
- * Pop a message box with a gived text and color
- */
-function msgAlert(text, color, hide = true) {
-    $('#msgAlert').stop()
-    $('#msgAlert').css('background-color', color)
-    $('#msgAlert').fadeTo(250, 1);
-    $('#msgAlert').text(text);
-    if (hide) {
-        $('#msgAlert').fadeOut(7500);
-    }
-}
+(function () {
 
-// BROWSER HACKS
-
-/**
- * Use regex over userAgent to check if the browser is Chrome based.
- */
-function isChrome() {
-    return /chrom(e|ium)/.test(navigator.userAgent.toLowerCase());
-}
-
-/**
- * Link buttons with sections with animation
- * and update url hash using 'pushState'.
- *
- * Smooth scroll only works on Chrome by default and jQuery bugs its behaviour
- * in Chrome but allow the effect in the rest of browsers.
- */
-function smoothScrollButton(buttonID, sectionID) {
-    if (!isChrome()) {
-        $(`[id*=${buttonID}]`).each((_, e) => {
-            $(e).click(() => {
-                $('html,body').animate({
-                    scrollTop: $(sectionID).offset().top
-                }, 'slow');
-                window.history.pushState({}, "", sectionID);
-            });
-        });
-    } else {
-        $(`[id*=${buttonID}]`).each((_, e) => {
-            $(e).attr("href", sectionID);
-        });
-    }
-}
-
-
-// SCROLL EVENTS
-
-/**
- * Set a fadeIn or fadeOut based on current scroll position.
- */
-function setFade(scrollPosToHide, selector) {
-    const time = 200;
-
-    (currScrollPos() > scrollPosToHide)
-        ? $(selector).fadeIn(time)
-        : $(selector).fadeOut(time);
-}
-
-/**
- * Append section id to the end of url, allowing to share specific section.
- */
-function setSectionToURL() {
-
-    const limit = currScrollPos() + 30;
-    const sections = ['#top', '#about', '#portfolio', '#contact', '#footer'];
-
-    for (const se of sections) {
-        if ((se !== window.location.hash) &&
-            $(se).offset().top < limit &&
-            $(se).offset().top + $(se).height() > limit) {
-            window.history.pushState({}, "", se);
-        }
-    }
-}
-
-
-// DOM MANIPULATION
-
-/**
- * Load imgs when is needed, not before.
- */
-function lazyLoadImgs() {
-    yall();
-}
-
-/**
- * For each 'video-container' tag generates desired html from tag's attributes
- */
-function populatePortfolio() {
-    $('video-container').each((_, e) => {
-
-        const img = $(e).attr("img") || "";
-        const ytID = $(e).attr("ytID") || "";
-        const slides = $(e).attr("slides") || "";
-        const gitRepo = $(e).attr("gitRepo") || "";
-        const vidTitle = $(e).attr("vidTitle") || "";
-        const vidDesc = $(e).attr("vidDesc") || "";
-        $(e).removeAttr("ytQ ytID slides gitRepo vidTitle vidDesc");
-
-
-        // Main elements
-        let mainDiv = $("<div>").addClass("video-description");
-        let iconSection = $("<section>").addClass("video-links");
-
-        // Youtube link and icon
-        if (ytID && ytID !== "") {
-            $(iconSection).append($("<a>")
-                .attr("href", `https://www.youtube.com/watch?v=${ytID}`)
-                .attr("target", "_blank").append($('<i>').addClass('icon-youtube-play'))
-            );
-        }
-
-        // Git link and icon
-        if (gitRepo && gitRepo !== "") {
-            $(iconSection).append($("<a>")
-                .attr("href", `${gitRepo}`)
-                .attr("target", "_blank").append($('<i>').addClass('icon-github-circled'))
-            );
-        }
-
-        // Slides link and icon
-        if (slides && slides !== "") {
-            $(iconSection).append($("<a>")
-                .attr("href", `${slides}`)
-                .attr("target", "_blank").append($('<i>').addClass('icon-doc-text-inv'))
-            );
-        }
-
-        // If exists 'git', 'youtube' or 'slides' links append icons section
-        if ((ytID && ytID !== "") || (gitRepo && gitRepo !== "") || (slides && slides !== "")) {
-            $(mainDiv).append(iconSection);
-        }
-
-        // Append thumb title and description
-        $(mainDiv)
-            .append($("<h3>")
-                .addClass("video-description-title")
-                .text(vidTitle)
-            )
-            .append($("<p>")
-                .addClass("video-description-content")
-                .text(vidDesc)
-            );
-
-        // Complete portfolio div
-        $(e)
-            .addClass("video neu-card")
-            .append($("<img>")
-                .addClass("lazy")
-                .addClass("video-thumb")
-                .attr('alt', vidTitle)
-                .attr('src', '/assets/img/thumbnail/placeholder.jpg')
-                .attr('data-src', ((img && img !== "") ? img : `https://img.youtube.com/vi/${ytID}/0.jpg`))
-            )
-            .append(mainDiv);
-    });
-}
-
-
-// F-BACKEND
-
-/**
- * Send an email using SmtpJS with your configured SMTP server
- */
-function sendMail() {
-
-    let fromName = $('#contact_fullname').val();
-    let from = $('#contact_email').val();
-    let subject = $('#contact_subject').val();
-    let body = $('#contact_body').val();
-
-    if (fromName === '' || from === '' || subject === '' || body === '') {
-        let emptyInputs = "⚠️ ";
-        if (fromName === '') { emptyInputs += 'Full name, '; }
-        if (from === '') { emptyInputs += 'Email, '; }
-        if (subject === '') { emptyInputs += 'Subject, '; }
-        if (body === '') { emptyInputs += 'Message, '; }
-        emptyInputs += 'must not be empty.'
-        msgAlert(emptyInputs, "#F7A859");
-        return;
-    }
-
-    let replied = false;
-    msgAlert("⏳ Connecting to the server.", "#5E5EBB", false);
-
-    setTimeout(_ => {
-        if (!replied) { msgAlert("⌛️ Time out. Sorry, try again later.", "#8E6EDD"); }
-    }, 3500);
-
-    Email.send({
-        SecureToken: "02c23bcd-40c2-48f9-9453-fd3a6a1a0cfd",
-        To: 'hello@cambalamas.com',
-        From: from,
-        FromName: fromName,
-        Subject: subject,
-        Body: body
-    }).then(status => {
-        replied = true;
-        if (status === 'OK') {
-            msgAlert("✅ Message sent successfully.", "#5EBB5E");
-        } else {
-            msgAlert("😕 " + status, "#BB5E5E");
-        }
-    });
-}
-
-// PRE-COMPUTED NODES
-
-const personalLinks = str2html(`
-        <section>
-            <a class="btn-link" href="https://www.instagram.com/cambalamas/" target="_blank">
-                <i class="icon-instagram"></i>
-            </a>
-            <a class="btn-link" href="https://twitter.com/cambalamas" target="_blank">
-                <i class="icon-twitter"></i>
-            </a>
-            <a class="btn-link" href="https://gitlab.com/users/cambalamas/projects" target="_blank">
-                <i class="icon-gitlab"></i>
-            </a>
-            <a class="btn-link" href="https://www.github.com/cambalamas/" target="_blank">
-                <i class="icon-github-circled"></i>
-            </a>
-        </section>
-
-        <section>
-            <a class="btn-link" href="https://www.linkedin.com/in/cambalamas/" target="_blank">
-                <i class="icon-linkedin"></i>
-            </a>
-            <a class="btn-link" href="mailto:hello@cambalamas.com?subject=Hi Daniel - Let’s chat about professional opportunities" target="_blank">
-                <i class="icon-mail-alt"></i>
-            </a>
-            <a class="btn-link" href="/assets/docs/Resume_DanielCamba.pdf" target="_blank">
-                <i class="icon-doc-text-inv"></i>
-            </a>
-        </section>
-    `);
-
-
-//
-//
-//
-// ON DOC READY
-
-$(document).ready(() => {
-
-    $('.personal-links').append(personalLinks);
-
-    $('#submitContact').click(sendMail);
-
-    $('#popNav').fadeOut(0);
-    let hamburgerOpen = false;
-    $('#btnHamburger, .pop-nav-btn').click(_ => {
-        if (hamburgerOpen) {
-            $('#popNav').fadeOut(100);
-        } else {
-            $('#popNav').fadeIn(100);
-        }
-        hamburgerOpen = !hamburgerOpen;
-    });
-
-    $(document).click(function (event) {
-        $target = $(event.target);
-        if (!$target.closest('#btnHamburger, #popNav').length) {
-            $('#popNav').fadeOut(200);
-            if (hamburgerOpen) { hamburgerOpen = false; }
-        }
-    });
-
-    const scrollEvents = [
-        setSectionToURL,
-        _ => setFade(100, '#btnTop'),
-        _ => setFade(100, '#btnHamburger'),
-        _ => {
-            if (currScrollPos() < 100) {
-                $('#popNav').fadeOut(200);
-                if (hamburgerOpen) { hamburgerOpen = false; }
-            }
+    /* CSS data improved access */
+    let CSS = {
+        _data: {
+            r: getComputedStyle(document.documentElement),
+            w: document.documentElement.style
         },
-    ];
-    for (const sEv of scrollEvents) { sEv(); }
-    for (const sEv of scrollEvents) { $(window).scroll(sEv); }
+        get: prop => CSS._data.r.getPropertyValue(prop),
+        set: (prop, val) => CSS._data.w.setProperty(prop, val),
+        rgba: (prop, opacity = 1.0) => `rgba(${CSS.get(prop)},${opacity})`
+    };
 
-    smoothScrollButton("btnAbout", "#about");
-    smoothScrollButton("btnPortfolio", "#portfolio");
-    smoothScrollButton("btnContact", "#contact");
-    smoothScrollButton("btnTop", "#top");
+    /* Setup smooth-scroll polyfill - disabled on chrome - */
+    let scroll = null;
+    if (!(/chrom(e|ium)/.test(navigator.userAgent.toLowerCase()))) {
+        scroll = new SmoothScroll('a[href*="#"]', {
+            header: '[data-scroll-header]',
+            easing: 'easeInOutQuint',
+        });
+    }
 
-    populatePortfolio();
+    /* Dynamic 'vh' unit for mobiles */
+    const vh = _ => {
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+    };
+    vh();
+    window.addEventListener('resize', vh);
 
-    lazyLoadImgs();
-});
+    /*  Intro animation and remove */
+    const intro = document.getElementById('intro');
+    const introText = document.getElementById('introText');
+    setTimeout(() => {
+        introText.style.textShadow = '0 0 25px rgba(var(--c-hmark),.85)';
+    }, 90);
+    setTimeout(() => {
+        introText.style.textShadow = '0 0 15px rgba(var(--c-hmark),.35)';
+    }, 690);
+    setTimeout(() => {
+        introText.style.textShadow = '0 0 25px rgba(var(--c-hmark),.85)';
+    }, 1290);
+    setTimeout(() => {
+        introText.style.textShadow = '0 0 15px rgba(var(--c-hmark),.35)';
+    }, 1890);
+    setTimeout(() => {
+        introText.style.transform = 'scale(0,0)';
+    }, 2690);
+    setTimeout(() => {
+        intro.style.opacity = '0';
+    }, 3090);
+    setTimeout(() => {
+        intro.style.display = 'none';
+        const topEl = document.querySelector('#top');
+        (scroll) ? scroll.animateScroll(topEl) : topEl.scrollIntoView();
+    }, 3490);
+
+    /* CONTACT logic */
+    document.getElementById('contactSubmit').addEventListener('click', _ => {
+
+        let timeouts = [];
+        const msgAlert = (msg, icon = '⚠️', color = CSS.rgba('--c-hmark', .85)) => {
+            for (const timeout of timeouts) { clearTimeout(timeout); }
+
+            const _contactSubmit = document.getElementById('contactSubmit');
+            const _alertBlock = document.getElementsByClassName('contact-alert')[0];
+            const _icon = document.getElementsByClassName('contact-alert-icon')[0];
+            const _msg = document.getElementsByClassName('contact-alert-msg')[0];
+
+            _alertBlock.style.backgroundColor = color;
+            _icon.textContent = icon;
+            _msg.textContent = msg;
+            _contactSubmit.style.opacity = 0;
+
+            timeouts.push(setTimeout(() => { _alertBlock.style.display = 'grid'; }, 300));
+            timeouts.push(setTimeout(() => { _alertBlock.style.opacity = 1; _contactSubmit.style.display = 'none'; }, 303));
+            timeouts.push(setTimeout(() => { _alertBlock.style.opacity = 0; }, 3000));
+            timeouts.push(setTimeout(() => { _alertBlock.style.display = 'none'; _contactSubmit.style.display = 'inherit'; }, 3350));
+            timeouts.push(setTimeout(() => { _contactSubmit.style.opacity = 1; }, 3400));
+        };
+
+        const fields = [
+            document.getElementById('contactFromName'),
+            document.getElementById('contactFrom'),
+            document.getElementById('contactSubject'),
+            document.getElementById('contactMsg')
+        ];
+        if (fields.some(el => el.value === '')) {
+            msgAlert('Must not be empty inputs.', '⚠️', '#d36f4e');
+            return;
+        }
+
+        msgAlert('Connecting to the server.', '⏳', '#4e6fd3');
+        const timeoutSMTP = setTimeout(_ => { msgAlert('Time out. Sorry, try again later.', '⌛️', '#4e6fa3'); }, 3500);
+
+        Email.send({
+            SecureToken: '02c23bcd-40c2-48f9-9453-fd3a6a1a0cfd',
+            To: 'hello@cambalamas.com',
+            FromName: fields[0].value,
+            From: fields[1].value,
+            Subject: fields[2].value,
+            Body: fields[3].value,
+        }).then(status => {
+            clearTimeout(timeoutSMTP);
+            if (status === 'OK') {
+                msgAlert('Message sent successfully.', '✅', '#428542');
+                fields.forEach(el => { el.value = ''; })
+            } else {
+                msgAlert(status, '😕');
+            }
+        });
+    });
+
+    /* Change url section on scroll */
+    window.addEventListener('scroll', _ => {
+        const currScrollPos = window.pageYOffset || document.documentElement.scrollTop;
+
+        const limit = currScrollPos + 30;
+        const sections = ['top', 'about', 'portfolio', 'contact', 'footer'];
+
+        for (const se of sections) {
+            const el = document.getElementById(se);
+            const top = el.getBoundingClientRect().top + currScrollPos;
+            if (('#' + se !== window.location.hash) && top < limit && top + el.scrollHeight > limit) { window.history.pushState({}, "", '#' + se); }
+        }
+    });
+
+    /* Cross-browser lazy load imgs */
+    yall();
+})();
